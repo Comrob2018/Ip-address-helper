094052245471835fb46a141557660ebe9ef0c99d
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2019-02-09 00:44:43 -0600

    Update function.py

--- function.py
+++ function.py
@@ -1,19 +1,23 @@
 import ipaddress
+
+'''
+This script takes user input for Ip v4 address and subnet mask. 
+The provided input then returns information for the class, available hosts in subnet and subnet id.
+It also checks the ip address and subnet mask for validity.
+It will also provide to the user their CIDR notation along with their 
+'''
+
 def ipv4addr(ip):
     '''
-    This Function takes user ip address input and determines what class network the ip address is. 
-    And based on that information they are given a list of available hosts, private address range and default subnet mask for the address provided.
+    This Function takes user ip address input and determines what class network the ip address is, 
+    and gives the user inforamtion about the network class and private address range for the 
+    address provided.
     '''
-    classA= ' is a Class A IP v4 address. The Class A network first octet range is from 0 to 127. Class A private address range is 10.0.0.0 to 10.255.255.255.'
-    classB= ' is a Class B IP v4 address. The Class B network first octet range is from 128 to 191. Class B private address range is 172.16.0.0 to 172.31.255.255. Additionally, there is the APIPA address range which is 169.254.0.1 through 169.254.255.254. This is for windows systems that are configured for DHCP without a server. These networks usually only have a few hosts on them. No more than 25 to 30.'
-    classC= ' is a Class C IP v4 address. The Class C network first octet range is from 192 to 223. Class C private address range is 192.168.0.0 to 192.168.255.255.'
-    classD= ' is a Class D IP v4 address. These addresses are used for multicast traffic and not available for hosts. Class D networks first octet range is from 224 to 239.'
-    classE= ' is a Class E IP v4 address. These addresses are reserved for future use and are not available for private or public use.  Class E networks first octet range is from 240 to 255.'
-    subnetA = ' Default subnet mask is 255.0.0.0. Available subnet masks for this address are from 255.0.0.0 to 255.255.255.255(This is for a point to point connection).'
-    subnetB = ' Default subnet mask is 255.255.0.0. Available subnet masks for this address are from 255.255.0.0 to 255.255.255.255(This is for a point to point connection).'
-    subnetC = ' Default subnet mask is 255.255.255.0. Available subnet masks for this address are from 255.255.255.0 to 255.255.255.255(This is for a point to point connection).'
-    subnetD = ' Default subnet mask for Class D networks is 255.255.255.255.'
-    subnetE = ' Default subnet mask for Class E networks is 255.255.255.255.'
+    classA= ' is a Class A IP v4 address. Class A private address range is 10.0.0.0 to 10.255.255.255.'
+    classB= ' is a Class B IP v4 address. Class B private address range is 172.16.0.0 to 172.31.255.255. Additionally, there is the APIPA address range which is 169.254.0.1 through 169.254.255.254. This is for systems that are configured for DHCP without a server.'
+    classC= ' is a Class C IP v4 address. Class C private address range is 192.168.0.0 to 192.168.255.255.'
+    classD= ' is a Class D IP v4 address. These addresses are used for multicast traffic and not available for hosts.'
+    classE= ' is a Class E IP v4 address.These addresses are reserved for future use and are not available for private or public use.'
     iplst = ip.split('.')
     for x in iplst:
         realip = '.'.join(iplst)
@@ -22,23 +26,23 @@ def ipv4addr(ip):
                 ipaddress.ip_address(realip)
                 break
             except ValueError:
-                return 'This is not a valid IP v4 address.'    
+                return 'This is not a valid IP v4 address.'
     for x in iplst:
-        if int(x) in range(0,256):
+        if int(x) in range(0,255):
             if len(x) < 4:
                 if int(x) in range(0, 128):
-                    return ip + classA + subnetA 
+                    return ip + classA 
                 elif int(x) in range(128, 192):
-                    return ip +  classB + subnetB
+                    return ip +  classB 
                 elif int(x) in range(192, 224):
-                    return ip +  classC + subnetC
+                    return ip +  classC 
                 elif int(x) in range(224, 240):
-                    return ip + classD + subnetD
-                elif int(x) in range(240, 256):
-                    return ip + classE + subnetE
+                    return ip + classD
+                elif int(x) in range(240, 255):
+                    return ip + classE
             else:
                 return 'This is not a valid IP v4 address.'
-      
+
 def subnetting(subnet):
     #This function determines if the subnet mask is valid.
     subnetcheck = [0,128,192,224,240,248,252,254,255]
@@ -47,9 +51,8 @@ def subnetting(subnet):
     for octet in subnetlist:
         x = bin(int(octet))
         num +=str(x[2::1])
-    global cidr 
+    global cidr
     cidr= num.count('1')
-
     if int(subnetlist[0]) == 255:
         if int(subnetlist[1]) in subnetcheck:
             if int(subnetlist[2]) in subnetcheck:
@@ -62,8 +65,8 @@ def subnetting(subnet):
         else:
             return 'This is not a valid subnet mask.'
     else:
-        return 'This is not a valid subnet mask.'                            
-    
+        return 'This is not a valid subnet mask.'
+
 def network_id(ip, cidr):
     #This returns the subnet id when given the ip address and the cidr notation.
     x = ip + '/' + cidr
@@ -73,17 +76,18 @@ def network_id(ip, cidr):
 
 def hostrange(ip, net_id):
     #This is to return the host ip address range for the ip address and network id provided.
+    #It also outputs all available host addressess to a file. 
     global hostrng
     hostrng = []
-    testfile = open('testfile.txt', "w")
+    iplist = open('iplist.txt', "w")
     for ip in ipaddress.ip_network(net_id):
         hostrng.append(str(ip))
     iplist.write('Here are your available hosts: '+ str(hostrng[1:-1]))
-    return 'You have '+ str(len(hostrng[1:-1])) +' host ip addresses available on this subnet. \nThe first useable host ip address is ' + hostrng[1] + '. \nThe last useable host ip address is ' + hostrng[-2] + '. \nYour broadcast IP address is ' + str(hostrng[-1]) + '.'
+    return 'There are '+ str(len(hostrng[1:-1])) +' host ip addresses available on this subnet. \nThe first useable host ip address is ' + hostrng[1] + '. \nThe last useable host ip address is ' + hostrng[-2] + '. \nYour broadcast IP address is ' + str(hostrng[-1]) + '.'
 
 ip=input('What is your IP v4 address? ')
 subnet=input('What is your subnet mask? ')
 print(ipv4addr(ip))
-print(subnetting(subnet)) 
+print(subnetting(subnet))
 print(network_id(ip, str(cidr)))
-print(hostrange(ip, net_id))  
+print(hostrange(ip, net_id))
